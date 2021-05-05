d2340cc623c3e2726e01566b0cacce88e81561ba
Marina <quriza@gmail.com>
2021-05-06 00:32:39 +0300

    resolve merge conflict

--- app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
@@ -34,10 +34,7 @@ class MovieDetailsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
