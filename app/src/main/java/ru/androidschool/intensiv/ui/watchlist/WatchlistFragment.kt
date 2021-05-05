ff3f8bc345d01d508635135e02bd9b9e376b52e6
Marina <quriza@gmail.com>
2021-05-06 00:14:58 +0300

    добавлена передача данных между фрагментами

--- app/src/main/java/ru/androidschool/intensiv/ui/watchlist/WatchlistFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/watchlist/WatchlistFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
@@ -12,7 +13,6 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.subjects.PublishSubject
 import io.reactivex.rxjava3.subjects.Subject
 import kotlinx.android.synthetic.main.fragment_watchlist.*
 import ru.androidschool.intensiv.R
@@ -20,17 +20,17 @@ import ru.androidschool.intensiv.data.Movie
 import ru.androidschool.intensiv.db.MlistWithMovies
 import ru.androidschool.intensiv.db.MovieDatabase
 import ru.androidschool.intensiv.ui.movie_details.MovieDetailsFragment
+import ru.androidschool.intensiv.ui.profile.SharedViewModel
+import ru.androidschool.intensiv.ui.profile.TabData
 import timber.log.Timber
 
 private const val TAB_NUM = "tab_num"
 
 class WatchlistFragment : Fragment() {
-    var subjectCount: Subject<Int> = PublishSubject.create()
 
 
-    // TODO: Rename and change types of parameters
     private var tabNum: Int = 0
-
+    private val model: SharedViewModel by activityViewModels()
     val compositeDisposable = CompositeDisposable()
 
     val adapter by lazy {
@@ -60,21 +60,21 @@ class WatchlistFragment : Fragment() {
         movies_recycler_view.adapter = adapter.apply { addAll(listOf()) }
 
 
+        val listKeys = resources.getStringArray(R.array.tab_keys)
+        compositeDisposable.add(
+            Observable.fromCallable({
+                val db = MovieDatabase.get(this.requireContext())
+                db.movieDao().getOneMListWithMovie(listKeys[this.tabNum])
+            }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    showLikedList(it)
+                }, {
+                    model.updateTabData(TabData(0, this.tabNum))
+                    Timber.e(it.message)
+                })
+        )
 
-        if (tabNum == 0) {
-            compositeDisposable.add(
-                Observable.fromCallable({
-                    val db = MovieDatabase.get(this.requireContext())
-                    db.movieDao().getOneMListWithMovie(MovieDetailsFragment.LIKED_LIST_KEY)
-                }).subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({
-                        showLikedList(it)
-                    }, {
-                        Timber.e(it.message)
-                    })
-            )
-        }
 
     }
 
@@ -86,7 +86,7 @@ class WatchlistFragment : Fragment() {
                 movie
             ) { movie -> }
         }.toList()
-        subjectCount.onNext(moviesList.count())
+        model.updateTabData(TabData(moviesList.count(), this.tabNum))
         movies_recycler_view.adapter = adapter.apply { addAll(moviesList) }
     }
 
